9184cb27f31406d0ac684f9eac04711d9d964ee6
pascaline <pascalinecte@gmail.com>
2024-08-17 23:46:19 +0200

    section price

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    new bootstrap.Carousel(document.getElementById('carouselContent'), {
+      interval: 3000,
+      pause: false
+    });
+  });
